b95f57986baa64723f697120526a30178a38c1e2
Guiisantoos <guiisantooos@gmail.com>
2022-06-07 00:50:30 -0300

    Terceira aula + ajustes do quadro de pixels, que canseira!

--- app.js
+++ app.js
@@ -1,52 +1,79 @@
 console.log ("Rodando a lsita em JS")
 
+const som_HIT = new Audio();
+som_HIT.src = './sons/efeitos_hit.wav'
+
 const sprites = new Image();
 sprites.src = 'Sprites.png'
 
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext ('2d')
 
-
-
-const passaro = {
-    spritex: 0,
-    spritey: 0,
-    altura: 35,
-    largura: 25,
-    x: 10,
-    y: 50,
-    telax: 25,
-    telay: 20,
-    velocidade: 0,
-    gravidade: 0.20,
-
-    atualiza(){
-        passaro.velocidade = passaro.velocidade + passaro.gravidade;
-        passaro.y = passaro.y + passaro.velocidade;
-    },
-
-    desenha(){
-        ctx.drawImage (sprites, 
-            passaro.spritex, passaro.spritey, 
-            passaro.altura, passaro.largura, 
-            passaro.x, passaro.y, 
-            passaro.telax, passaro.telay)
-        
+function bate(passaro, chao){
+    if(passaro.y + passaro.ajusteChao >= chao.y ){
+        return true;
     }
+    return false;
 }
+
+function criaPassaro(){
+    const passaro = {
+        spritex: 0,
+        spritey: 0,
+        altura: 33,
+        largura: 24,
+        x: 10,
+        y: 70,
+        telax: 25,
+        telay: 20,
+        ajusteChao: 27,
+        velocidade: 0,
+        gravidade: 0.25,
+        pulo: 5,
+    
+        pula(){
+            passaro.velocidade = - passaro.pulo;
+        },
+        atualiza(){
+    
+        if(bate(passaro, chao)){
+            som_HIT.play();
+
+            setTimeout(() => {
+                mudaDeTela(Telas.inicio);
+
+            }, 500)
+            return; 
+        }
+            passaro.velocidade = passaro.velocidade + passaro.gravidade;
+            passaro.y = passaro.y + passaro.velocidade;
+        },
+    
+        desenha(){
+            ctx.drawImage (sprites, 
+                passaro.spritex, passaro.spritey, 
+                passaro.altura, passaro.largura, 
+                passaro.x, passaro.y, 
+                passaro.telax, passaro.telay)
+            
+        }
+    }
+return passaro;
+}
+
 const chao = {
     spritex: 0,
-    spritey: 609,
-    altura: 224,
-    largura: 113,
+    spritey: 610,
+    largura: 224,
+    altura: 112,
     x: 0,
-    y: canvas.height - 113,
+    y: canvas.height - 96,
     desenha(){
         ctx.drawImage(sprites, 
             chao.spritex, chao.spritey, 
-            chao.altura, chao.largura, 
+            chao.largura, chao.altura, 
             chao.x, chao.y,
-            chao.altura, chao.largura);
+            chao.largura, chao.altura);
 
         ctx.drawImage(sprites, 
             chao.spritex, chao.spritey, 
@@ -100,13 +127,62 @@ const mensagemComeço ={
     }
 }
 
+const globais = {};
+let telaAtiva = {};
+function mudaDeTela(novaTela){
+   telaAtiva = novaTela;
+
+   if(telaAtiva.inicializa){
+       telaAtiva.inicializa();
+   }
+}
+const Telas = {
+    inicio: {
+        inicializa(){
+            globais.passaro = criaPassaro();
+        },
+        desenha(){
+            planoDeFundo.desenha();
+            chao.desenha();
+            globais.passaro.desenha();
+            mensagemComeço.desenha();
+            
+        },
+        atualiza(){
+        },
+        click(){
+            mudaDeTela(Telas.jogo)
+        }
+    }
+};
+
+Telas.jogo = {
+    desenha(){
+        planoDeFundo.desenha();
+        chao.desenha();
+        globais.passaro.desenha();
+    },
+
+    click(){
+        globais.passaro.pula();
+    },
+    atualiza(){
+        globais.passaro.atualiza();
+    }
+}
+
 function loop (){
-    planoDeFundo.desenha();
-    chao.desenha();
-    passaro.desenha();
-    mensagemComeço.desenha();
-    passaro.atualiza();
+
+    telaAtiva.desenha();
+    telaAtiva.atualiza();
     requestAnimationFrame(loop);
 }
 
+window.addEventListener('click', function(){
+    if(telaAtiva.click()){
+        mudaDeTela(Telas.jogo)
+    }
+})
+
+mudaDeTela(Telas.inicio);
 loop();
